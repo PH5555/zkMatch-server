c40b90130eb6dffab2a649a2ce99c54e43c160ed
PH5555 <lmkn5342@gmail.com>
2025-07-23 10:48:59 +0900

    first commit

---
+++ src/main/java/com/zkrypto/zkMatch/ZkMatchApplication.java
@@ -0,0 +1,13 @@
+package com.zkrypto.zkMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkrypto/zkMatch/ZkMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkrypto.zkMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
